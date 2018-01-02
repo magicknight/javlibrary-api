203b40031eca330f6db6a56e84a5c37610865dd4
trazyn <tn.razy@gmail.com>
2018-01-02 22:06:39 +0800

    Add language support

--- utils/request.js
+++ utils/request.js
@@ -4,14 +4,19 @@ import HttpsProxyAgent from 'https-proxy-agent';
 import _request from 'request-promise';
 
 function getInstance() {
+    var lang = process.env.lang || 'en';
     var agent;
+
+    if (['en', 'jp', 'tw', 'cn'].indexOf(lang) === -1) {
+        lang = 'en';
+    }
 
     if (process.env.http_proxy) {
         agent = new HttpsProxyAgent(url.parse(process.env.http_proxy));
     }
 
     return _request.defaults({
-        baseUrl: 'http://www.javlibrary.com/cn/',
+        baseUrl: `http://www.javlibrary.com/${lang}/`,
         headers: {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
         },
